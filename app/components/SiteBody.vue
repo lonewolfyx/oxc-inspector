80800fdc8d26b403a854509aa0e9795bf7f50cfc
lonewolfyx <877085893@qq.com>
2026-03-09 16:37:22 +0800

    style(theme): update background colors to use slate palette

--- app/components/SiteBody.vue
+++ app/components/SiteBody.vue
@@ -1,13 +1,13 @@
 <template>
     <Body class="antialiased flex h-full text-base text-foreground bg-zinc-100">
         <div class="absolute inset-0 z-0">
-            <div class="relative h-full w-full bg-[#f8fafc] dark:bg-[#0f172a]">
+            <div class="relative h-full w-full bg-slate-50 dark:bg-slate-900">
                 <div
                     :class="cn(
                         'absolute bottom-0 left-0 right-0 top-0',
                         'bg-[size:16px_16px]',
-                        'bg-[radial-gradient(#0000001a_1px,#f8fafc_1px)]',
-                        'dark:bg-[radial-gradient(#ffffff33_1px,#0f172a_1px)]',
+                        'bg-[radial-gradient(#0000001a_1px,theme(colors.slate.50)_1px)]',
+                        'dark:bg-[radial-gradient(#ffffff33_1px,theme(colors.slate.900)_1px)]',
                     )"
                 />
             </div>
