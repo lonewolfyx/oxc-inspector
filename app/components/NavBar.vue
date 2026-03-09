c1d4c1b6f943464c38137aa488b85fb06e990652
lonewolfyx <877085893@qq.com>
2026-03-09 20:34:33 +0800

    feat(navbar): add project navigation buttons to header

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -1,17 +1,37 @@
 <template>
-    <div class="flex justify-between items-center **:data-[slot=separator]:!h-4">
-        <span
-            :class="cn(
-                'text-2xl',
-                'font-mono font-extralight font-stretch-condensed',
-            )"
-        >
-            Oxidation Compiler Config Inspector
-        </span>
-        <div class="flex items-center gap-3">
-            <GitHubLink />
-            <Separator orientation="vertical" />
-            <ModeSwitcher />
+    <div class="flex flex-col gap-4">
+        <div class="flex justify-between items-center **:data-[slot=separator]:!h-4">
+            <span
+                :class="cn(
+                    'text-2xl',
+                    'font-mono font-extralight font-stretch-condensed',
+                )"
+            >
+                Oxidation Compiler Config Inspector
+            </span>
+            <div class="flex items-center gap-3">
+                <GitHubLink />
+                <Separator orientation="vertical" />
+                <ModeSwitcher />
+            </div>
+        </div>
+        <div class="flex justify-start items-center gap-4">
+            <Button class="text-base capitalize rounded bg-slate-50 dark:bg-slate-900 shadow-none" variant="outline">
+                <Icon mode="svg" name="ph:diamonds-four-duotone" />
+                Project
+            </Button>
+            <Button class="text-base capitalize rounded bg-slate-50 dark:bg-slate-900 shadow-none" variant="outline">
+                <Icon mode="svg" name="ph:stack-duotone" />
+                Configs
+            </Button>
+            <Button class="text-base capitalize rounded bg-slate-50 dark:bg-slate-900 shadow-none" variant="outline">
+                <Icon mode="svg" name="ph:list-dashes-duotone" />
+                rules
+            </Button>
+            <Button class="text-base capitalize rounded bg-slate-50 dark:bg-slate-900 shadow-none" variant="outline">
+                <Icon mode="svg" name="ph:paint-brush-broad-fill" />
+                oxfmt
+            </Button>
         </div>
     </div>
 </template>
