4e51edc92f089ef7cc18964cd6fcca30e87fef16
lonewolfyx <877085893@qq.com>
2026-03-09 15:41:48 +0800

    style(SiteBody): format div element with proper line breaks

--- app/components/SiteBody.vue
+++ app/components/SiteBody.vue
@@ -2,7 +2,9 @@
     <Body class="antialiased flex h-full text-base text-foreground bg-zinc-100">
         <div class="absolute inset-0 z-0">
             <div class="relative h-full w-full bg-[#f8fafc]">
-                <div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(#0000001a_1px,#f8fafc_1px)] bg-[size:16px_16px] " />
+                <div
+                    class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(#0000001a_1px,#f8fafc_1px)] bg-[size:16px_16px] "
+                />
             </div>
         </div>
         <div class="flex grow flex-col z-10">
