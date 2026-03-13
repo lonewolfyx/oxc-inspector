4c16ba56d4b8670d0a0fe18c443ed071715ced67
lonewolfyx <877085893@qq.com>
2026-03-14 00:06:25 +0800

    style(layout): remove background grid pattern and unused utility import

--- app/components/SiteBody.vue
+++ app/components/SiteBody.vue
@@ -1,17 +1,17 @@
 <template>
     <Body class="antialiased flex h-full text-base text-foreground bg-slate-50 dark:bg-slate-900 overflow-hidden">
-        <div class="absolute inset-0 z-0">
-            <div class="relative h-full w-full bg-slate-50 dark:bg-slate-900">
-                <div
-                    :class="cn(
-                        'absolute bottom-0 left-0 right-0 top-0',
-                        'bg-[size:16px_16px]',
-                        'bg-[radial-gradient(#0000001a_1px,theme(colors.slate.50)_1px)]',
-                        'dark:bg-[radial-gradient(#ffffff33_1px,theme(colors.slate.900)_1px)]',
-                    )"
-                />
-            </div>
-        </div>
+        <!--        <div class="absolute inset-0 z-0"> -->
+        <!--            <div class="relative h-full w-full bg-slate-50 dark:bg-slate-900"> -->
+        <!--                <div -->
+        <!--                    :class="cn( -->
+        <!--                        'absolute bottom-0 left-0 right-0 top-0', -->
+        <!--                        'bg-[size:16px_16px]', -->
+        <!--                        'bg-[radial-gradient(#0000001a_1px,theme(colors.slate.50)_1px)]', -->
+        <!--                        'dark:bg-[radial-gradient(#ffffff33_1px,theme(colors.slate.900)_1px)]', -->
+        <!--                    )" -->
+        <!--                /> -->
+        <!--            </div> -->
+        <!--        </div> -->
         <div class="flex grow flex-col z-10 px-4 lg:px-14 py-8 lg:py-10 space-y-8 overflow-auto">
             <slot />
         </div>
@@ -19,8 +19,6 @@
 </template>
 
 <script lang="ts" setup>
-import { cn } from '~/lib/utils'
-
 defineOptions({
     name: 'SiteBody',
 })
