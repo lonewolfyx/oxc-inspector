0d1935a6389d7666a89f96744801bfe9c76565bb
lonewolfyx <877085893@qq.com>
2026-03-13 13:40:20 +0800

    feat(config): update ConfigInspectorBadge to use dynamic GitHub repo URL

--- app/components/ConfigInspectorBadge.vue
+++ app/components/ConfigInspectorBadge.vue
@@ -3,11 +3,11 @@
         <NuxtLink
             :class="cn(
                 'flex items-center gap-2 mr-1',
-                'text-sm capitalize',
+                'capitalize',
                 'font-mono font-extralight font-stretch-condensed',
             )"
             target="_blank"
-            to="https://github.com/eslint/config-inspector"
+            :to="`https://github.com/${app.github.repo}`"
         >
             <img alt="" class="size-8 inline-block" src="/favicon.svg"> {{ app.title }}
         </NuxtLink>
